527c7bb9e8ff5610931c9185686d277eb1e6d8d3
Megha-Joshi <megha24joshi@gmail.com>
2022-06-28 17:42:29 +0530

    initial-setup

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore} from "@firebase/firestore"
+import { getFirestore} from "@firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBjfQa_LPswCA85jtJ5uiiiJ1GVLQ-_4cI",
@@ -14,3 +15,4 @@ const firebaseConfig = {
   const app = initializeApp(firebaseConfig);
 
   export const db = getFirestore(app);
+  export const auth = getAuth(app);
