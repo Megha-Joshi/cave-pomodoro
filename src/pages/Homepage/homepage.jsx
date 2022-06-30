a205139078c09c7f9055e1e9b0125f19ac4756a5
Megha-Joshi <megha24joshi@gmail.com>
2022-06-30 18:03:17 +0530

    feat: CRUD operation

--- src/pages/Homepage/homepage.jsx
+++ src/pages/Homepage/homepage.jsx
@@ -3,10 +3,31 @@ import { Navbar } from "../../components/Navbar/navbar";
 import "./homepage.css";
 import Modal from 'react-modal';
 import { Link } from "react-router-dom";
+import { useTask } from "../../context/task-context";
 
 const Homepage = () =>{
 
 const [modal, setModal] = useState(false);
+const [currTask, setCurrTask] = useState({});
+const { tasks, createTask, editTask, singleTask, setSingleTask, editing, setEditing, deleteTask} = useTask();
+
+const editTaskHandler = (task) =>{
+    setModal(true);
+    setSingleTask({...singleTask, name: task.name, duration: task.duration, description: task.description})
+    setCurrTask(task);
+    setEditing(true);
+}
+
+const taskHandler = (task) =>{
+    if(editing){
+        editTask(task.id);
+        setSingleTask({name: "", duration: "", description: ""});
+        setEditing(false);
+    }
+    else{
+        createTask();
+    }
+}
 const customStyle = {
     overlay: {
       top: "6rem",
@@ -29,13 +50,16 @@ return(
         </section>
         <section className="tasks-container">
             <p className="tasks-head">Tasks</p>
-            <div className="tasks-list">
-                <li className="task-name">Exercise</li>
+
+            {tasks.map((task) => (
+                <div className="tasks-list">
+                <li className="task-name">{task.name}</li>
                 <div className="task-icon">
-                    <span><i class="fas fa-edit icon-color"></i></span>
-                    <span><i class="fas fa-trash-alt icon-color"></i></span>
+                    <span><i class="fas fa-edit icon-color" onClick={() => editTaskHandler(task)}></i></span>
+                    <span><i class="fas fa-trash-alt icon-color" onClick={() => deleteTask(task.id)}></i></span>
                 </div>
-            </div>
+                </div> 
+            ))}
             <div className="tasks-btn">
             <button class="float-btn float-action" onClick={() => setModal(true)}>
                     <span><i class="fal fa-plus"></i></span>
@@ -51,12 +75,12 @@ return(
                         <i class="fas fa-times icon-color" onClick={() => setModal(false)}></i>
                     </header>
                     <main className="modal-subhead">
-                        <input type="text" placeholder="Task Title" className="inp-box"/>
-                        <textarea type="text" placeholder="Task Description" className="inp-box"></textarea>
-                        <input type="number" placeholder="Time (minutes)" className="inp-box"/>
-                        <Link to="/timer">
-                        <button className="btn btn-info btn-text btn-modal">CREATE</button>
-                        </Link>
+                        <input type="text" placeholder="Task Title" className="inp-box" value={singleTask.name} onChange={(e) => setSingleTask({...singleTask, name: e.target.value})}/>
+                        <textarea type="text" placeholder="Task Description" className="inp-box" value={singleTask.description} onChange={(e) => setSingleTask({...singleTask, description: e.target.value})}></textarea>
+                        <input type="number" placeholder="Time (minutes)" className="inp-box" value={singleTask.duration} onChange={(e) => setSingleTask({...singleTask, duration: e.target.value})}/>
+                        {/* <Link to="/timer"> */}
+                        <button className="btn btn-info btn-text btn-modal" onClick={() => taskHandler(currTask)}>CREATE</button>
+                        {/* </Link> */}
                     </main>
                 </Modal>
             )
