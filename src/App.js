8126243e8b07ed31ca6bf0e5d8c997d9e3f671de
Megha-Joshi <megha24joshi@gmail.com>
2022-07-01 17:01:19 +0530

    update: removed-unused-routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import { Login } from './pages/login-signup/login';
-import { Signup } from './pages/login-signup/signup';
-import { Navbar } from './components/Navbar/navbar';
 import { Landingpage } from './pages/Landingpage/landingpage';
 import { Homepage } from './pages/Homepage/homepage';
 import { Timer } from './pages/Timer/timer';
@@ -15,8 +12,6 @@ function App() {
         <Route path="/" element={<Landingpage />}/>
         <Route path="/home" element={<Homepage />}/>
         <Route path="/timer/:taskID" element={<Timer />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/signup" element={<Signup />}/>
       </Routes>
     </div>
   );
